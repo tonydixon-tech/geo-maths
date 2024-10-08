4c6e93154bf9126f89b628593d8570061629dd22
Tony <anthonydixon56@yahoo.com>
2024-10-08 16:39:41 +0100

    first two tests written

--- GeoMaths.Test/GeoMaths_should.cs
+++ GeoMaths.Test/GeoMaths_should.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using GeoMaths.Types;
 
 namespace GeoMaths.Test
@@ -6,7 +7,6 @@ namespace GeoMaths.Test
     [TestFixture]
     public class GeoMaths_should
     {
-
         private IGeoMaths _geoMaths;
         [SetUp]
         public void Setup()
@@ -26,16 +26,37 @@ namespace GeoMaths.Test
             Assert.Pass();
         }
 
-        [Test]
-        public void TestHDistance_coord_coord()
+        [TestCaseSource(nameof(HaversineGeoCoordCases))]
+        public void TestHDistance_coord_coord(GeoCoord start, GeoCoord finish)
         {
             Console.WriteLine("Testing HDistance");
-            GeoCoord start = new GeoCoord(0.0, 0.0);
-            GeoCoord finish = new GeoCoord(0.0, 1.0);
             double actual = _geoMaths.HDistance(start, finish);
-            double expected = 0;
-            Assert.That(actual, Is.EqualTo(expected));
+            double expected = Constants.METRES_PER_NM * 60.0;
+            Assert.That(actual, Is.EqualTo(expected).Within(200.0), "not remotely equal");
         }
+
+
+        [TestCase(0.0, 0.0, 1.0, 0.0, 0.0, 0.01)]
+        [TestCase(0.0, 0.0, 1.0, 1.0, 45.0, 0.01)]
+        [TestCase(0.0, 0.0, 0.0, 1.0, 90.0, 0.01)]
+        [TestCase(0.0, 0.0, -1.0, 1.0, 135.0, 0.01)]
+        [TestCase(0.0, 0.0, -1.0, 0.0, 180.0, 0.01)]
+        [TestCase(0.0, 0.0, -1.0, -1.0, 225.0, 0.01)]
+        [TestCase(0.0, 0.0, 0.0, -1.0, 270.0, 0.01)]
+        [TestCase(0.0, 0.0, 1.0, -1.0, 315.0, 0.01)]
+        public void TestHeading(double lat0, double lon0, double lat1, double lon1, double expected, double delta)
+        {
+            Console.WriteLine("Testing Heading");
+            double actual = _geoMaths.Heading(new GeoCoord(lat0, lon0), new GeoCoord(lat1, lon1));
+            Assert.That(actual, Is.EqualTo(expected).Within(delta));
+        }
+
+        public static object[] HaversineGeoCoordCases =
+        {
+        new object[] { new GeoCoord(1.0, 0.0), new GeoCoord(0.0, 0.0) },
+        new object[] { new GeoCoord(0.0, 0.0), new GeoCoord(1.0, 0.0) }
+        };
     }
+
 
 }
--- GeoMaths/Constants.cs
+++ GeoMaths/Constants.cs
@@ -11,6 +11,7 @@ namespace GeoMaths
         public static readonly double EARTH_SEMI_MINOR_AXIS = 6356752.3142;
         public static readonly double ECCENTRICITY_SQUARED = 6.69437999014 * 0.001;
 
-        public static readonly int IDX_DEFAULT = -1;        
+        public static readonly int IDX_DEFAULT = -1;
+        public static readonly double METRES_PER_NM = 1852.0;        
     }
 }
