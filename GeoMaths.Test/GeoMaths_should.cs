91b420c31bd2bbf48251e4d473faba5e9122cacf
Tony <anthonydixon56@yahoo.com>
2024-10-09 10:17:13 +0100

    tests added for calculating new position

--- GeoMaths.Test/GeoMaths_should.cs
+++ GeoMaths.Test/GeoMaths_should.cs
@@ -14,17 +14,19 @@ namespace GeoMaths.Test
             _geoMaths = new GeoMaths();
         }
 
-        [Test]
-        public void TestCalcPosition_deg_dist_ref()
+        [TestCaseSource(nameof(CalcPositionBearingDistanceCases))]
+        public void TestCalcPosition_deg_dist_ref(double degrees, double nm_distance, GeoCoord reference, GeoCoord expected, double delta)
         {
-            Assert.Pass();
+            GeoCoord actual = _geoMaths.CalcPosition(degrees, nm_distance, reference);
+            Assert.That(actual.lat, Is.EqualTo(expected.lat).Within(delta), "expected latitude is incorrect");
+            Assert.That(actual.lng, Is.EqualTo(expected.lng).Within(delta), "expected longitude is incorrect");
         }
 
         [TestCaseSource(nameof(CalcPositionNorthEastCases))]
         public void TestCalcPosition_primary_north_east(GeoCoord primary, double nm_north, double nm_east, GeoCoord expected, double delta)
         {
             GeoCoord actual = _geoMaths.CalcPosition(primary, nm_north, nm_east);
-            //double expected = 60.0;
+
             Assert.That(actual.lat, Is.EqualTo(expected.lat).Within(delta), "expected latitude is incorrect");
             Assert.That(actual.lng, Is.EqualTo(expected.lng).Within(delta), "expected longitude is incorrect");
         }
@@ -70,6 +72,19 @@ namespace GeoMaths.Test
         new object[] { new GeoCoord(0.0, 0.0), -100.0d, -100.0d, new GeoCoord(-1.66, -1.66), 0.02d },// 100 nm south and west
         new object[] { new GeoCoord(0.0, 0.0), 0.0d, -100.0d, new GeoCoord(0.0, -1.66), 0.02d },     // 100 nm west
         new object[] { new GeoCoord(0.0, 0.0), 100.0d, -100.0d, new GeoCoord(1.66, -1.66), 0.02d }   // 100 nm north and west
+        };
+
+        
+        public static object[] CalcPositionBearingDistanceCases =
+        {
+        new object[] {  0.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(1.66, 0.0), 0.02d },         // 100 nm north
+        new object[] {  45.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(1.18, 1.18), 0.02d },       // 100 nm north-east
+        new object[] {  90.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(0.0, 1.66), 0.02d },        // 100 nm east
+        new object[] {  135.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(-1.18, 1.18), 0.02d },     // 100 nm south-east
+        new object[] {  180.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(-1.66, 0.0), 0.02d },      // 100 nm south
+        new object[] {  225.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(-1.18, -1.18), 0.02d },    // 100 nm south-west
+        new object[] {  270.0d, 100.0d, new GeoCoord(0.0, 0.0),new GeoCoord(0.0, -1.66), 0.02d },       // 100 nm west
+        new object[] {  315.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(1.18, -1.18), 0.02d }      // 100 nm north-west
         };
     }
 
