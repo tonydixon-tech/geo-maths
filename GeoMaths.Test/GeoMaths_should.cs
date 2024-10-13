bc5d875e7f5fcd62804775a687315e89d2790bf4
Tony <anthonydixon56@yahoo.com>
2024-10-13 11:32:28 +0100

    simplified unit tests

--- GeoMaths.Test/GeoMaths_should.cs
+++ GeoMaths.Test/GeoMaths_should.cs
@@ -41,51 +41,53 @@ namespace GeoMaths.Test
         }
 
 
-        [TestCase(0.0, 0.0, 1.0, 0.0, 0.0, 0.01d)]
-        [TestCase(0.0, 0.0, 1.0, 1.0, 45.0, 0.01d)]
-        [TestCase(0.0, 0.0, 0.0, 1.0, 90.0, 0.01d)]
-        [TestCase(0.0, 0.0, -1.0, 1.0, 135.0, 0.01d)]
-        [TestCase(0.0, 0.0, -1.0, 0.0, 180.0, 0.01d)]
-        [TestCase(0.0, 0.0, -1.0, -1.0, 225.0, 0.01d)]
-        [TestCase(0.0, 0.0, 0.0, -1.0, 270.0, 0.01d)]
-        [TestCase(0.0, 0.0, 1.0, -1.0, 315.0, 0.01d)]
-        public void TestHeading_shouldReturnHeadingToCoordinateFromReferenceCoordinate(double lat0, double lon0, double lat1, double lon1, double expected, double delta)
+        [TestCase(1.0, 0.0, 0.0, 0.01d)]
+        [TestCase(1.0, 1.0, 45.0, 0.01d)]
+        [TestCase(0.0, 1.0, 90.0, 0.01d)]
+        [TestCase(-1.0, 1.0, 135.0, 0.01d)]
+        [TestCase(-1.0, 0.0, 180.0, 0.01d)]
+        [TestCase(-1.0, -1.0, 225.0, 0.01d)]
+        [TestCase(0.0, -1.0, 270.0, 0.01d)]
+        [TestCase(1.0, -1.0, 315.0, 0.01d)]
+        public void TestHeading_shouldReturnHeadingToCoordinateFromReferenceCoordinate(double lat1, double lon1, double expected, double delta)
         {
             Console.WriteLine("Testing Heading {0} deg", expected);
-            double actual = _geoMaths.Heading(new GeoCoord(lat0, lon0), new GeoCoord(lat1, lon1));
+            GeoCoord start = new GeoCoord();
+            GeoCoord finish =  new GeoCoord(lat1, lon1);
+            double actual = _geoMaths.Heading(start, finish);
             Assert.That(actual, Is.EqualTo(expected).Within(delta));
         }
 
         public static object[] HaversineGeoCoordCases =
         {
-            new object[] { new GeoCoord(0.0, 0.0), new GeoCoord(0.0, 0.0), 0.0d, 0.001d },
-            new object[] { new GeoCoord(1.0, 0.0), new GeoCoord(0.0, 0.0), 60.0d, 0.2d },
-            new object[] { new GeoCoord(0.0, 0.0), new GeoCoord(1.0, 0.0), 60.0d, 0.2d }
+            new object[] { new GeoCoord(), new GeoCoord(), 0.0d, 0.001d },
+            new object[] { new GeoCoord(1.0, 0.0), new GeoCoord(), 60.0d, 0.2d },
+            new object[] { new GeoCoord(), new GeoCoord(1.0, 0.0), 60.0d, 0.2d }
         };
 
         public static object[] CalcPositionNorthEastCases =
         {
-            new object[] { new GeoCoord(0.0, 0.0), 100.0d, 0.0d, new GeoCoord(1.66, 0.0), 0.02d },       // 100 nm north
-            new object[] { new GeoCoord(0.0, 0.0), 100.0d, 100.0d, new GeoCoord(1.66, 1.66), 0.02d },    // 100 nm north and east
-            new object[] { new GeoCoord(0.0, 0.0), 0.0d, 100.0d, new GeoCoord(0.0, 1.66), 0.02d },       // 100 nm east
-            new object[] { new GeoCoord(0.0, 0.0), -100.0d, 100.0d, new GeoCoord(-1.66, 1.66), 0.02d },  // 100 nm south and east
-            new object[] { new GeoCoord(0.0, 0.0), -100.0d, 0.0d, new GeoCoord(-1.66, 0.0), 0.02d },     // 100 nm south
-            new object[] { new GeoCoord(0.0, 0.0), -100.0d, -100.0d, new GeoCoord(-1.66, -1.66), 0.02d },// 100 nm south and west
-            new object[] { new GeoCoord(0.0, 0.0), 0.0d, -100.0d, new GeoCoord(0.0, -1.66), 0.02d },     // 100 nm west
-            new object[] { new GeoCoord(0.0, 0.0), 100.0d, -100.0d, new GeoCoord(1.66, -1.66), 0.02d }   // 100 nm north and west
+            new object[] { new GeoCoord(), 100.0d, 0.0d, new GeoCoord(1.66, 0.0), 0.02d },       // 100 nm north
+            new object[] { new GeoCoord(), 100.0d, 100.0d, new GeoCoord(1.66, 1.66), 0.02d },    // 100 nm north and east
+            new object[] { new GeoCoord(), 0.0d, 100.0d, new GeoCoord(0.0, 1.66), 0.02d },       // 100 nm east
+            new object[] { new GeoCoord(), -100.0d, 100.0d, new GeoCoord(-1.66, 1.66), 0.02d },  // 100 nm south and east
+            new object[] { new GeoCoord(), -100.0d, 0.0d, new GeoCoord(-1.66, 0.0), 0.02d },     // 100 nm south
+            new object[] { new GeoCoord(), -100.0d, -100.0d, new GeoCoord(-1.66, -1.66), 0.02d },// 100 nm south and west
+            new object[] { new GeoCoord(), 0.0d, -100.0d, new GeoCoord(0.0, -1.66), 0.02d },     // 100 nm west
+            new object[] { new GeoCoord(), 100.0d, -100.0d, new GeoCoord(1.66, -1.66), 0.02d }   // 100 nm north and west
         };
 
 
         public static object[] CalcPositionBearingDistanceCases =
         {
-            new object[] {  0.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(1.66, 0.0), 0.02d },         // 100 nm north
-            new object[] {  45.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(1.18, 1.18), 0.02d },       // 100 nm north-east
-            new object[] {  90.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(0.0, 1.66), 0.02d },        // 100 nm east
-            new object[] {  135.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(-1.18, 1.18), 0.02d },     // 100 nm south-east
-            new object[] {  180.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(-1.66, 0.0), 0.02d },      // 100 nm south
-            new object[] {  225.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(-1.18, -1.18), 0.02d },    // 100 nm south-west
-            new object[] {  270.0d, 100.0d, new GeoCoord(0.0, 0.0),new GeoCoord(0.0, -1.66), 0.02d },       // 100 nm west
-            new object[] {  315.0d, 100.0d, new GeoCoord(0.0, 0.0), new GeoCoord(1.18, -1.18), 0.02d }      // 100 nm north-west
+            new object[] {  0.0d, 100.0d, new GeoCoord(), new GeoCoord(1.66, 0.0), 0.02d },         // 100 nm north
+            new object[] {  45.0d, 100.0d, new GeoCoord(), new GeoCoord(1.18, 1.18), 0.02d },       // 100 nm north-east
+            new object[] {  90.0d, 100.0d, new GeoCoord(), new GeoCoord(0.0, 1.66), 0.02d },        // 100 nm east
+            new object[] {  135.0d, 100.0d, new GeoCoord(), new GeoCoord(-1.18, 1.18), 0.02d },     // 100 nm south-east
+            new object[] {  180.0d, 100.0d, new GeoCoord(), new GeoCoord(-1.66, 0.0), 0.02d },      // 100 nm south
+            new object[] {  225.0d, 100.0d, new GeoCoord(), new GeoCoord(-1.18, -1.18), 0.02d },    // 100 nm south-west
+            new object[] {  270.0d, 100.0d, new GeoCoord(),new GeoCoord(0.0, -1.66), 0.02d },       // 100 nm west
+            new object[] {  315.0d, 100.0d, new GeoCoord(), new GeoCoord(1.18, -1.18), 0.02d }      // 100 nm north-west
         };
     }
 
